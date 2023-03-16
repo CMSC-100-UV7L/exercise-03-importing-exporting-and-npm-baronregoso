d57c75a1f0f84aea60405a03b27853cae53f7722
Baron Regoso <bdregoso@up.edu.ph>
2023-03-16 23:39:06 +0800

    Feat: Able to generate unique id and write to file when input is valid; Test: Added one test case for test runs

--- function.js
+++ function.js
@@ -3,12 +3,26 @@ import { v4 as uuid } from 'uuid'; // do not forget to install the said modules 
 import validator from 'validator'; // some modules are able to import default packages while some are not capable
 
 function generateUniqueID(fname, lname){
-    
+    var uniqueID = uuid().substring(0,8); 
+
+    return (fname[0].toLowerCase()+lname.toLowerCase()+uniqueID); 
+
 }
 
 function addAccount(arr){
-    
+    if(arr.length == 4){ 
+        if(arr[0] != "" && arr[1] != "" && arr[2] != ""){ 
+            if(validator.isEmail(arr[2])){ 
+                if(arr[arr.length-1] >= 18){ 
+                    
+                    let str = arr.join(",") + "," + generateUniqueID(arr[0], arr[1])
+                    appendFileSync("users.txt", str+"\n"); 
+                    return true;
+                } 
+            }
+        }
+    }
+    return false;
 }
 
-
-
+addAccount(["Alan", "Turing", "aturing@w3c.com", 58]);
