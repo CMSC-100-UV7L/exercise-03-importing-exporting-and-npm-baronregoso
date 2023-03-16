464ab19359d3997ca9dca2279366c8e1fd4a5be9
Baron Regoso <bdregoso@up.edu.ph>
2023-03-16 23:33:08 +0800

    Chore: added and imported files in function.js

---
+++ function.js
@@ -0,0 +1,14 @@
+import { appendFileSync } from 'node:fs'; // import from modules
+import { v4 as uuid } from 'uuid'; // do not forget to install the said modules first
+import validator from 'validator'; // some modules are able to import default packages while some are not capable
+
+function generateUniqueID(fname, lname){
+    
+}
+
+function addAccount(arr){
+    
+}
+
+
+
